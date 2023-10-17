aafb2ea5be9ca3fa512c2c38a27fab4461e46ceb
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2023-10-17 22:24:34 +0900

    :heavy_plus_sign: 定義

--- server/main.rb
+++ server/main.rb
@@ -5,8 +5,66 @@ get '/questionnaires/sample' do
   json(
     {
       id: 1,
-      name: 'アンケートサンプル',
-      is_open: true
+      title: 'アンケートサンプル',
+      is_open: true,
+      questions: [
+        {
+          code: 'select_food',
+          title: '好きな食べもの',
+          description: '好きな食べ物を選択してください',
+          is_required: true,
+          answer_type: 'radio_button',
+          options: [
+            {
+              code: 'code1',
+              title: 'パン'
+            },
+            {
+              code: 'code2',
+              title: 'ごはん'
+            },
+            {
+              code: 'code3',
+              title: 'うどん'
+            }
+          ]
+        },
+        {
+          code: 'select_food_multiple',
+          title: '好きな食べもの複数選択',
+          description: '好きな食べ物を選択してください',
+          is_required: true,
+          answer_type: 'checkbox',
+          options: [
+            {
+              code: 'code1',
+              title: 'パン'
+            },
+            {
+              code: 'code2',
+              title: 'ごはん'
+            },
+            {
+              code: 'code3',
+              title: 'うどん'
+            }
+          ]
+        },
+        {
+          code: 'select_food_free',
+          title: '好きな食べもの',
+          description: '好きな食べ物を入力してください',
+          is_required: true,
+          answer_type: 'short_text'
+        },
+        {
+          code: 'select_food_free_long',
+          title: '好きな食べもの長文',
+          description: '好きな食べ物を入力してください',
+          is_required: true,
+          answer_type: 'long_text'
+        }
+      ]
     }
   )
 end
