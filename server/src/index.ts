5c73e70e9dcb1ad4fb887baeeb91f6226c25a243
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2023-10-23 14:47:18 +0900

    :pencil2: cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,34 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 
 import { sampleQuestionnaire, sampleResult } from "./sample";
 
 const app = new Hono();
 app.use("*", logger());
 
-app.get("/api/v1/questionnaires/sample/builder", (c) => c.json(sampleQuestionnaire));
-app.put("/api/v1/questionnaires/sample/builder", (c) => c.json(sampleQuestionnaire));
-app.get("/api/v1/questionnaires/sample/form", (c) => c.json(sampleQuestionnaire));
+// 開発用
+app.use(
+  "/api/*",
+  cors({
+    origin: "*",
+    allowHeaders: ["*"],
+    allowMethods: ["*"],
+    exposeHeaders: ["*"],
+    maxAge: 600,
+  })
+);
+
+app.get("/api/v1/questionnaires/sample/builder", (c) =>
+  c.json(sampleQuestionnaire)
+);
+app.put("/api/v1/questionnaires/sample/builder", (c) =>
+  c.json(sampleQuestionnaire)
+);
+app.get("/api/v1/questionnaires/sample/form", (c) =>
+  c.json(sampleQuestionnaire)
+);
 app.get("/api/v1/questionnaires/sample/result", (c) => c.json(sampleResult));
 
 serve({
