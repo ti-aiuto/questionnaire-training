eaa529c83551db42e5a8b4d234ed331905996353
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2023-10-23 18:23:25 +0900

    :pencil2: 実装

--- frontend/vue2/src/pages/Result.vue
+++ frontend/vue2/src/pages/Result.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
-    <p>Hello Result Page</p>
+  <div v-if="questionnaireResult">
+    <h2>{{ questionnaireResult.questionnaire.title }}</h2>
+
+    <div
+      v-for="aggregatedQuestion in questionnaireResult.aggregated_questions"
+    >
+      <div>
+        {{ aggregatedQuestion.question.title }}
+      </div>
+
+      <div 
+        v-for="aggregatedOption in aggregatedQuestion.aggregated_options"
+      >
+        {{ aggregatedOption.label }}: 
+        {{ aggregatedOption.count }}<br>
+      </div>
+      <br>
+    </div>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
+import ky from "ky";
 
-export default Vue.extend({});
+export default Vue.extend({
+  async created() {
+    this.questionnaireResult = await ky(
+      "http://localhost:8787/api/v1/questionnaires/sample/result"
+    ).json();
+  },
+  data() {
+    return {
+      questionnaireResult: null,
+    };
+  },
+});
 </script>
