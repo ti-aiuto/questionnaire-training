f0a2d2f6aa0001a4b6f3735495fdd74d1a06bd06
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2023-10-23 14:07:40 +0900

    :pencil2: 修正

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,86 +1,16 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import { logger } from 'hono/logger'
+import { logger } from "hono/logger";
 
-import {
-  question1,
-  question2,
-  question3,
-  question4,
-  sampleQuestionnaire,
-} from "./sample";
+import { sampleQuestionnaire, sampleResult } from "./sample";
 
 const app = new Hono();
-app.use('*', logger())
+app.use("*", logger());
 
-app.get("/questionnaires/sample/form", (c) => c.json(sampleQuestionnaire));
-app.get("/questionnaires/sample/result", (c) =>
-  c.json({
-    questionnaire: sampleQuestionnaire,
-    aggregated_questions: [
-      {
-        question: question1,
-        aggregated_options: [
-          {
-            label: "うどん",
-            count: 2,
-          },
-          {
-            label: "ごはん",
-            count: 3,
-          },
-          {
-            label: "パン",
-            count: 5,
-          },
-        ],
-      },
-      {
-        question: question2,
-        aggregated_options: [
-          {
-            label: "うどん",
-            count: 2,
-          },
-          {
-            label: "ごはん",
-            count: 3,
-          },
-          {
-            label: "パン",
-            count: 5,
-          },
-        ],
-      },
-      {
-        question: question3,
-        aggregated_options: [
-          {
-            label: "うどんが好きです",
-            count: 1,
-          },
-          {
-            label: "ごはんが好きです",
-            count: 1,
-          },
-        ],
-      },
-      {
-        question: question4,
-        aggregated_options: [
-          {
-            label: "うどんが好きです",
-            count: 1,
-          },
-          {
-            label: "ごはんが好きです",
-            count: 1,
-          },
-        ],
-      },
-    ],
-  })
-);
+app.get("/api/v1/questionnaires/sample/builder", (c) => c.json(sampleQuestionnaire));
+app.post("/api/v1/questionnaires/sample/builder", (c) => c.json(sampleQuestionnaire));
+app.get("/api/v1/questionnaires/sample/form", (c) => c.json(sampleQuestionnaire));
+app.get("/api/v1/questionnaires/sample/result", (c) => c.json(sampleResult));
 
 serve({
   fetch: app.fetch,
--- server/src/sample.ts
+++ server/src/sample.ts
@@ -69,3 +69,69 @@ export const sampleQuestionnaire = {
   is_open: true,
   questions: [question1, question2, question3, question4],
 };
+
+export const sampleResult = {
+  questionnaire: sampleQuestionnaire,
+  aggregated_questions: [
+    {
+      question: question1,
+      aggregated_options: [
+        {
+          label: "うどん",
+          count: 2,
+        },
+        {
+          label: "ごはん",
+          count: 3,
+        },
+        {
+          label: "パン",
+          count: 5,
+        },
+      ],
+    },
+    {
+      question: question2,
+      aggregated_options: [
+        {
+          label: "うどん",
+          count: 2,
+        },
+        {
+          label: "ごはん",
+          count: 3,
+        },
+        {
+          label: "パン",
+          count: 5,
+        },
+      ],
+    },
+    {
+      question: question3,
+      aggregated_options: [
+        {
+          label: "うどんが好きです",
+          count: 1,
+        },
+        {
+          label: "ごはんが好きです",
+          count: 1,
+        },
+      ],
+    },
+    {
+      question: question4,
+      aggregated_options: [
+        {
+          label: "うどんが好きです",
+          count: 1,
+        },
+        {
+          label: "ごはんが好きです",
+          count: 1,
+        },
+      ],
+    },
+  ],
+};
