ce8b77f0e91f7f69f2ad92524117cb26f9ad91e2
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2023-10-24 10:18:31 +0900

    :pencil2: 修正

--- frontend/vue2/src/pages/Home.vue
+++ frontend/vue2/src/pages/Home.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    <p>Hello Home Page</p>
+    <ul>
+      <li>
+        <router-link to="/form" v-slot="{ href }">
+          <a :href="href">回答画面</a>
+        </router-link>
+      </li>
+
+      <li>
+        <router-link to="/result" v-slot="{ href }">
+          <a :href="href">結果画面</a>
+        </router-link>
+      </li>
+
+      <li>
+        <router-link to="/builder" v-slot="{ href }">
+          <a :href="href">編集画面</a>
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
