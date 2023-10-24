14e442e6df9c0e5223a3be289252fb28c6a94582
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2023-10-24 11:12:42 +0900

    :pencil2: 修正

--- frontend/vue2/src/pages/Builder.vue
+++ frontend/vue2/src/pages/Builder.vue
@@ -16,6 +16,28 @@
             <option value="long_text">自由記述（複数行）</option>
           </select>
         </div>
+
+        <template
+          v-if="
+            question.answer_type === 'radio_button' ||
+            question.answer_type === 'checkbox'
+          "
+        >
+          <div v-for="option in question.options">
+            表記: <input type="text" :value="option.label" /><br />
+            識別子: <input type="text" :value="option.code" /><br />
+
+            <button>選択肢を削除</button>
+            <button>↑へ</button>
+            <button>↓へ</button>
+            <br>
+            <br>
+          </div>
+
+          <button>選択肢を追加</button>
+        </template>
+
+        <br />
       </div>
     </div>
   </div>
@@ -36,7 +58,7 @@ export default Vue.extend({
   data() {
     return {
       questionnaire: null,
-      editingQuestionnaire: null
+      editingQuestionnaire: null,
     };
   },
 });
